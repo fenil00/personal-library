48c7b1b3b8905cfa696204244554c54ba03e2c5f
fenil00 <fenildesai00@gmail.com>
2022-09-06 19:27:00 +0200

    DELETE book route added

--- index.js
+++ index.js
@@ -3,28 +3,37 @@ const app = express();
 const path = require('path');
 const books = require('./fakedata');
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname,'/views'));
 app.set('view engine', 'ejs');
 
 // POST Routes
-app.post('/books', (req, res)=> {
+app.post('/books/newbook', (req, res)=> {
     const {title, author, comment, isbn} = req.body;
     const comments = new Array(comment);
     books.books.push({_id : uuidv4(), title: title, author : author, ISBN:isbn,comments : comments })
-    res.redirect('books');
+    res.redirect('/books');
 });
 
-app.post('/books/:id/addnewcomment', (req, res)=> {
+app.post('/books/:id/newcomment', (req, res)=> {
     const { id } =  req.params;
     const { comment } = req.body;
     const b  = books.books.find(i => i._id == id);
     b.comments.push(comment);
     
     res.redirect(`/books/${id}`);
+});
+
+app.delete('/books/:id', (req, res)=> {
+    const { id } =  req.params;
+    const b  = books.books.find(i => i._id == id);
+    books.books.pop(b);
+    
+    res.redirect(`/books`);
 });
 
 // GET Routes
